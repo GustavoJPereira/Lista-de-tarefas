90d585cdcd115f1ef763cb412628545dabd49d79
Escravo <gustavojosepereira2021@gmail.com>
2021-10-05 21:46:50 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-
+import * as C from './App.styles';
 
 const App = () => {
   return (
-
+    <C.Container>
+      <C.Area>
+        <C.Header>
+          Lista de tarefas
+        </C.Header>
+      </C.Area>
+    </C.Container>
   );
 }
 
